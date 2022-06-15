520af6b00b6f5301128a0203418895fe1d23b76f
Franekskc <franek5432@gmail.com>
2022-06-15 10:15:37 +0200

    zmiana nazw

---
+++ src/main/java/object/entity/Anvil.java
@@ -0,0 +1,32 @@
+package object.entity;
+
+import main.EventAnvil;
+import main.GamePanel;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Anvil extends Entity {
+
+    GamePanel gp;
+    public Anvil(GamePanel gp){
+        super(gp);
+
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/images/anvil.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void interactPlayer(boolean c) {
+        if (c && !gp.player.isInvisible) {
+            gp.player.isInvisible = true;
+            new EventAnvil(gp, gp.armor, gp.weapon);
+        }
+    }
+}
+
+
+
